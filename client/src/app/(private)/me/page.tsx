b5daa5ddbcc6a01ea1b96225927140a415650eb5
anhvd001 <anhvd@nal.vn>
2024-05-27 21:28:56 +0700

    change text

--- client/src/app/(private)/me/page.tsx
+++ client/src/app/(private)/me/page.tsx
@@ -19,7 +19,7 @@ export default async function MeProfile() {
 
   return (
     <div>
-      <h1 className='text-xl font-semibold text-center'>Profile asdf</h1>
+      <h1 className='text-xl font-semibold text-center'>Profile edit</h1>
       <div className="flex justify-center">
         <ProfileForm profile={result.payload.data} />
       </div>
