616b08ae58b57ecf0b48321de2f3092e9dd39abc
anhvd001 <anhvd@nal.vn>
2024-05-29 16:13:12 +0700

    update Login 222222

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -21,7 +21,7 @@ export default function Login() {
     <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
       <div className='flex flex-col space-y-2 text-center'>
         <h1 className='text-2xl font-semibold tracking-tight'>Đăng nhập 1</h1>
-        <h1 className='text-2xl font-semibold tracking-tight'>Đăng nhập</h1>
+        <h1 className='text-2xl font-semibold tracking-tight'>Đăng nhập 222</h1>
       </div>
       <div className="grid gap-7">
         <LoginForm />
